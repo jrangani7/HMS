38b5d4879c0b2ee1a6ee0a2c71545cebdc8dda39
Jishnu Rangani <jrangani7@gmail.com>
2020-06-28 20:10:25 +0530

    bugs fixxed- form undefined

--- application/routes.py
+++ application/routes.py
@@ -1,11 +1,11 @@
 from application import app
 from flask import render_template,request,session
-from .forms import LoginForm
+from application.forms import LoginForm
 
 
 @app.route('/')
 def index():
-    return render_template("login.html")
+    return render_template("index.html")
 
 @app.route('/login')
 def login():
