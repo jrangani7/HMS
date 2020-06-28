b1ff677a44d94b1eaf9c557a31ff4bac82190160
Nikhil Nimbalkar <nsv.nimbalkar@gmail.com>
2020-06-28 18:37:31 +0530

    Added Login template to and forms.py to craate new forms

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,18 @@
 from application import app
 from flask import render_template,request,session
+from .forms import LoginForm
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/login')
+def login():
+    form=LoginForm()
+    return render_template("login.html",form=form)
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
 
