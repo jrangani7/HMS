0f767b03172df38ffa228ca5ea04f01dacd3ccc0
Jishnu Rangani <jrangani7@gmail.com>
2020-06-28 21:33:52 +0530

    Login implemented. UI for flash messages in login.html remaining

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from application import app
-from flask import render_template,request,session
+from flask import render_template,request,session ,flash,redirect,url_for
 from application.forms import LoginForm
-
+from application import mysql
 
 @app.route('/')
 def index():
@@ -16,7 +16,15 @@ def login():
     if request.method == 'POST':
         username=request.form["username"]
         password=request.form["password"]
-        return render_template("home.html",username=username,password=password)
-
+        n= mysql.connect().cursor().execute("SELECT * from userstore where loginid =%s and password=%s",(username,password))
+        if n :
+            if 'AD' in username:
+                return render_template("desk/index.html",username=username,password=password)
+            elif 'PH' in username:
+                return render_template("pharmacy/index.html",username=username,password=password)
+            return render_template("diagnostic/index.html",username=username,password=password)
+        else:
+            flash(' Invalid Credentials.')
+            return redirect(url_for('login'))
     form=LoginForm()
     return render_template("login.html",form=form)
