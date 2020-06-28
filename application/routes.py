6fb09da9c1c5cc6b94863f58a3178f70ddf18b8a
Sakar Koot <43310631+sakarkoot@users.noreply.github.com>
2020-06-28 21:50:44 +0530

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -2,20 +2,20 @@ from application import app
 from flask import render_template,request,session ,flash,redirect,url_for
 from application.forms import LoginForm
 from application import mysql
+from datetime import timedelta
+app.permanent_session_lifetime = timedelta(minutes=30)
 
 @app.route('/')
 def index():
     return render_template("index.html")
-
-@app.route('/home')
-def home():
-    return render_template("home.html")
 
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         username=request.form["username"]
         password=request.form["password"]
+        session["username"] = username
+        session.permanent = True
         n= mysql.connect().cursor().execute("SELECT * from userstore where loginid =%s and password=%s",(username,password))
         if n :
             if 'AD' in username:
@@ -28,3 +28,17 @@ def login():
             return redirect(url_for('login'))
     form=LoginForm()
     return render_template("login.html",form=form)
+
+@app.route('/logout')
+def logout():
+    session.pop("username",None)
+    form=LoginForm()
+    return render_template("login.html",form=form)
+
+@app.route('/home')
+def home():
+    if "username" in session:
+        return render_template("home.html",username=session['username'])
+    else:
+        form=LoginForm()
+        return render_template("login.html",form=form)
