01f11ae91b38df28b26979c70a2dcfe8f5c61610
Rudra Malali <rudra.malali@gmail.com>
2020-06-29 21:53:58 +0530

    updated delete functionality

--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField,PasswordField,SubmitField,DateField,SelectField,IntegerField
+from wtforms.validators import DataRequired,NumberRange
 
+
+#####################################################################################
 class LoginForm(FlaskForm):
     username=StringField('username',validators=[DataRequired()])
     password=PasswordField('password',validators=[DataRequired()])
+
+#####################################################################################
+# Patient Registration Form #
+class PatientRegistrationForm(FlaskForm):
+    pName=StringField('name',validators=[DataRequired()])
+    pAge=IntegerField('age',validators=[DataRequired(),NumberRange(min=1,max=100)])
+    dateOfSubmission=DateField('date',format='%d-%m-%Y')
+    bedType=SelectField('bed',validators=[DataRequired()],choices=['General','Semi Sharing','Single'])
+    address=StringField('address')
+    city=StringField('city')
+    state=StringField('state')
+    status=SelectField('status',validators=[DataRequired()],choices=['Active','Discharged'])
+    
+class DeleteForm(FlaskForm):
+    pid=IntegerField('patID')
